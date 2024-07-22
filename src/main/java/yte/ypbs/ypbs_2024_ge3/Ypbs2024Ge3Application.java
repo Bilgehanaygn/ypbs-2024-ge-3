1ec58d22d6d1636f863954aa2c1f8a9b25b8ec77
Ali Buğra ŞAHIN <alibugra.sahin@tubitak.gov.tr>
2024-07-22 10:29:26 +0300

    Backend Project Initialized

---
+++ src/main/java/yte/ypbs/ypbs_2024_ge3/Ypbs2024Ge3Application.java
@@ -0,0 +1,13 @@
+package yte.ypbs.ypbs_2024_ge3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ypbs2024Ge3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ypbs2024Ge3Application.class, args);
+	}
+
+}
