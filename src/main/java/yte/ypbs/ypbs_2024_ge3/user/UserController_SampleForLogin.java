2eeaf9d4988901acd2b412a885fff99a394b31e3
Serkan YILMAZ <serkan.yilmaz@tubitak.gov.tr>
2024-07-24 10:20:31 +0300

    Organizasyon User'a Kurumsal->Birim 'ine eklendi. Userdaki entitylere BaseEntity extentionı eklendi

---
+++ src/main/java/yte/ypbs/ypbs_2024_ge3/user/UserController_SampleForLogin.java
@@ -0,0 +1,18 @@
+package yte.ypbs.ypbs_2024_ge3.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users/login")
+@RequiredArgsConstructor
+public class UserController_SampleForLogin {
+
+    @GetMapping
+    public String login(@RequestParam final String username,
+                        final String password) {
+        return "Username: " + username + "\nPassword: " + password;
+    }
+
+
+}
