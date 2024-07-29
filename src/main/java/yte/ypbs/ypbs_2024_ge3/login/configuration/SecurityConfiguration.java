632b67b1e1de8fad0132632bede58d4d9ab6d2e0
Serkan YILMAZ <serkan.yilmaz@tubitak.gov.tr>
2024-07-29 16:54:49 +0300

    YPBS-250-login user login controlleri yapıldı

--- src/main/java/yte/ypbs/ypbs_2024_ge3/login/configuration/SecurityConfiguration.java
+++ src/main/java/yte/ypbs/ypbs_2024_ge3/login/configuration/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package yte.ypbs.ypbs_2024_ge3.login.configuration;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -13,7 +17,11 @@ import org.springframework.security.web.context.DelegatingSecurityContextReposit
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 import yte.ypbs.ypbs_2024_ge3.login.service.CustomAuthenticationProvider;
+
+import java.io.IOException;
 
 
 @Configuration
@@ -26,6 +34,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/auth/userHeader").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
@@ -44,5 +53,18 @@ public class SecurityConfiguration {
         return new DelegatingSecurityContextRepository(new RequestAttributeSecurityContextRepository(), new HttpSessionSecurityContextRepository());
     }
 
+    @Component
+    public class CorsFilter extends OncePerRequestFilter {
 
+        @Override
+        protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+            response.addHeader("Access-Control-Allow-Origin", "*");
+            response.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH, HEAD");
+            response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+            response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, Access-Control-Allow-Credentials");
+            response.addHeader("Access-Control-Allow-Credentials", "true");
+            response.addIntHeader("Access-Control-Max-Age", 10);
+            filterChain.doFilter(request, response);
+        }
+    }
 }
---
+++ src/main/java/yte/ypbs/ypbs_2024_ge3/user/response/UserHeaderResponse.java
@@ -0,0 +1,6 @@
+package yte.ypbs.ypbs_2024_ge3.user.response;
+
+public record UserHeaderResponse(String name,
+                                 String surname,
+                                 byte[] photo) {
+}
