fa7c8e29b63ddfb19b749ca507654a1729615894
Serkan YILMAZ <serkan.yilmaz@tubitak.gov.tr>
2024-08-02 16:36:03 +0300

    YPBS-256 Unversioned Files added

---
+++ src/main/java/yte/ypbs/ypbs_2024_ge3/user/response/UserDataResponse.java
@@ -0,0 +1,10 @@
+package yte.ypbs.ypbs_2024_ge3.user.response;
+
+public record UserDataResponse(String isimSoyisim,
+                               String unvan,
+                               String gorev,
+                               String birim,
+                               String proje,
+                               String email,
+                               String telefon) {
+}
